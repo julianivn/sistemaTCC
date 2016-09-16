44039f447a28caca77616129a8fc015fe42f2d93
Eder Soares <edersoares@me.com>
2016-09-16 00:05:18 -0300

    Utilizando rotas GET e POST

--- public/index.php
+++ public/index.php
@@ -3,6 +3,10 @@
 // Inclui o arquivo de autoload do Composer
 require_once __DIR__.'/../vendor/autoload.php';
 
+// Utilizando componentes
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 // Instância uma aplicação Silex
 $app = new Silex\Application();
 
@@ -11,7 +15,21 @@ $app['debug'] = true;
 
 // Define a rota para a raiz do site quando o método for GET
 $app->get('/', function () {
-    return 'sistemaTCC';
+    return "sistemaTCC\n";
+});
+
+// Exemplos de rotas
+$app->get('/creditos/', function () {
+    return "Créditos: Alunos do Curso de Ciência da Computação\n";
+});
+$app->get('/aluno/{nome}/', function ($nome) {
+    return "O nome do aluno é {$nome}\n";
+});
+$app->get('/login/', function () {
+    return "Login\n";
+});
+$app->post('/login/', function (Request $request) {
+    return new Response('Obrigado pela visita: ' . $request->get('nome') . "\n", 201);
 });
 
 // Dispara a aplicação
