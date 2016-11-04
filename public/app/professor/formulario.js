fc8a87461f3be5f8eb68a071d2479edc6021a6d2
Eric Quadros <ericquadros7@gmail.com>
2016-11-04 13:09:29 -0200

    Diferenciando mensagem de Incluso ou Alterado

--- public/app/professor/formulario.js
+++ public/app/professor/formulario.js
@@ -30,7 +30,7 @@ $(function() {
 
         const url = restURL + (itemID ? itemID + '/' : '' );
         const method = itemID ? 'put' : 'post';
-
+        const text = itemID ? 'Alterado': 'Incluso';
         const request = $.ajax({
                 url: url,
                 type: method,
@@ -43,7 +43,7 @@ $(function() {
             verifyErrors();
             swal({
                 title: "OK",
-                text: "Alterado!",
+                text: text,
                 type: "success",
                 showCancelButton: false,
                 confirmButtonText: "Voltar para Lista",
