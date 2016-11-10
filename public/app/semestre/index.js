1491b984c9ca1199342006d23b390147062a637e
Ricardo Martins <ricardo@servipec.com.br>
2016-11-10 12:41:17 -0200

    Update index.js

--- public/app/semestre/index.js
+++ public/app/semestre/index.js
@@ -57,7 +57,7 @@
 
     const body = {
 	  dataini: $formSemestre.find('#dataini').val(),
-	  dataini: $formSemestre.find('#datafim').val(),
+	  dataifim: $formSemestre.find('#datafim').val(),
       campus: $formSemestre.find('#campus').val(),
       ano: $formSemestre.find('#ano').val(),
       semestre: $formSemestre.find('#semestre').val()
