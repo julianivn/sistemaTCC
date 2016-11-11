a2bef69444d41537222a1d3f11c8a4c38ced7f25
Eder Soares <edersoares@me.com>
2016-11-11 21:22:18 -0200

    Login: mensagens de erro

--- src/SistemaTCC/Controller/IndexController.php
+++ src/SistemaTCC/Controller/IndexController.php
@@ -3,14 +3,21 @@
 namespace SistemaTCC\Controller;
 
 use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
 
 class IndexController {
 
-    public function indexAction(Application $app) {
-        return $app['twig']->render('login/index.twig');
+    public function indexAction(Application $app, Request $request) {
+        return $app['twig']->render('login/index.twig', [
+			'erroDeLogin' => $app['security.last_error']($request)
+		]);
     }
 
     public function creditosAction(Application $app) {
+		return $app['twig']->render('index/creditos.twig');
+    }
+
+    public function gerarSenhaAction(Application $app) {
 
 		$token = $app['security.token_storage']->getToken();
 
@@ -19,10 +26,6 @@ class IndexController {
 		}
 
 		return 'admin';
-    }
-
-    public function loginAction(Application $app) {
-
     }
 
 }
